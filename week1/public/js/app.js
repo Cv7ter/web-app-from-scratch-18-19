4b9eddcf3f7f5ac91dca896d3ab0e78ecee970e3
Cv7ter <36409253+Cv7ter@users.noreply.github.com>
2019-02-07 21:12:12 +0100

    New version

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -1,26 +1,40 @@
 var request = new XMLHttpRequest();
 //var api = "https://api.chucknorris.io/jokes/random";
 //var api = "https://blokweb.org/stories";
-var api = "https://api.unsplash.com/photos?client_id=44318672e19eef622d1b4830b84f2a7162b0507b1d170245134a5cc23986759a";
+//var api = "https://api.unsplash.com/photos?client_id=44318672e19eef622d1b4830b84f2a7162b0507b1d170245134a5cc23986759a";
+var home = "https://api.unsplash.com/photos?client_id=44318672e19eef622d1b4830b84f2a7162b0507b1d170245134a5cc23986759a";
+var id = "client_id=44318672e19eef622d1b4830b84f2a7162b0507b1d170245134a5cc23986759a"
+var ruser = "";
 
-request.onreadystatechange = function() {
+
+request.onreadystatechange = function() { // .addeventlistener
     if (this.readyState == 4 && this.status == 200) {
+
        // Typical action to be performed when the document is ready:
        var data = JSON.parse(request.responseText);
        console.log(data);
 
-       var i;
-       for(i=0; i<data.length; i++) {
-         image = data[i]['urls']['full'];
-         
-       }
-       image = data[0]['urls']['full'];
-
-       document.getElementById("demo").setAttribute("src", image);
-       // document.querySelector("main").innerHTML = image;
-       //document.getElementById("demo").src = data.icon_url;
-       //document.querySelector(".demo") < kan je alles pakken
+    var fotonr = 0;
+    while(fotonr < data.length) {
+       image = data[fotonr]['urls']['small'];
+       console.log(image);
+        document.getElementById(fotonr).setAttribute("src", image);
+       document.getElementById(fotonr).addEventListener("click", getDetail);
+       fotonr++
     }
+
+    function getDetail()  { // uit de scope van event handling
+//        name = data[this.id]['user']['name'];
+        user = data[this.id]['user']['username'];
+       ruser = user; 
+var api = "https://api.unsplash.com/users/" + ruser + "/photos?" + id;
+       request.open("GET", api, true);
+       request.send();
+    }
+
+  }
+
 };
-request.open("GET", api, true);
+
+request.open("GET", home, true);
 request.send();
